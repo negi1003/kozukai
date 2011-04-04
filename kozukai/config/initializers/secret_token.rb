725e0f660b9617a89858f5aadd4416e35a34cab3
kawabata <hiroto_kawabata@indexokinawa.co.jp>
2011-04-04 22:07:26 +0900

    アプリ作り直し

---
+++ kozukai/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kozukai::Application.config.secret_token = '7118af2ae0484d3f71e63562536334107b49d6b4de5731f1c1b99637a5f0e39aa24156c21c09a0c8c9c0175479e6aaeb52e457daec354a9c2c1a938481cd6015'
