9aee4eeffb2291b123d7928def5197189ec4b960
kawabata <hiroto_kawabata@indexokinawa.co.jp>
2011-04-04 21:50:06 +0900

    applicationの作り直し

--- kozukai/app/models/user.rb
+++ kozukai/app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+   
+=begin
+  validates :name, :presence => true,
+                   :length => { :within => 4..8 }
+=end
 end
--- trunk/app/controllers/users_controller.rb
+++
@@ -1,4 +0,0 @@
-class Users::SessionsController < Devise::SessionsController
-  devise_for :users, :controllers => { :sessions => "users/sessions" }
-end
-
