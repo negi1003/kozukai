32ab2454c9c3cc780463f64391e955ba14e72364
川畑 拓人 <hiroto@kawahata-takuto-no-MacBook-Pro.local>
2011-05-14 17:39:56 +0900

    calender_helper導入

--- kozukai/config/environment.rb
+++ kozukai/config/environment.rb
@@ -4,3 +4,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Kozukai::Application.initialize!
+
+$yobitbl =['日','月','火','水','木','金','土']
+$tsukitbl = ['','１月','２月','３月','４月','５月','６月','７月','８月','９月','10月','11月','12月']
+
